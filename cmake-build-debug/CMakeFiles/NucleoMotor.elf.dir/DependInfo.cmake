78be75c71d408e971148683506a14d0c3e881524
morswin499 <152016295+morswin499@users.noreply.github.com>
2025-05-16 19:40:31 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/NucleoMotor.elf.dir/DependInfo.cmake
@@ -0,0 +1,69 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Core/Startup/startup_stm32f446retx.s" "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/cmake-build-debug/CMakeFiles/NucleoMotor.elf.dir/Core/Startup/startup_stm32f446retx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F446xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Core/Inc"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Core/Src/gpio.c" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/gpio.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Core/Src/main.c" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/main.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Core/Src/stm32f4xx_it.c" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Core/Src/syscalls.c" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/syscalls.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Core/Src/sysmem.c" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/sysmem.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Core/Src/system_stm32f4xx.c" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Core/Src/tim.c" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/tim.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Core/Src/usart.c" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Core/Src/usart.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "/Users/morswin/STM32_projects/MiniMotorboard/NucleoMotor/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj" "gcc" "CMakeFiles/NucleoMotor.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/NucleoMotor.elf.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for NucleoMotor.elf.
+# This may be replaced when dependencies are built.
