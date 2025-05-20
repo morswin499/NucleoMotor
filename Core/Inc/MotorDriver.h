b4d2c4574b6a03a344b2a5f141dd673b9033686c
benji <beniamin.raczynski@gmail.com>
2025-05-20 18:15:09 +0200

    h

--- Core/Inc/MotorDriver.h
+++ Core/Inc/MotorDriver.h
@@ -5,4 +5,8 @@
 #ifndef MOTORDRIVER_H
 #define MOTORDRIVER_H
 void forward(uint8_t speed);
+void backword(uint8_t speed);
+void turnLeft(uint8_t speed);
+void turnRight(uint8_t speed);
+void stop();
 #endif //MOTORDRIVER_H
